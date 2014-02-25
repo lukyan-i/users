9a940795baad3a22cf23b787a0940c85eee2cff4
lukjanenko <lukyan-i@yandex.ua>
2014-02-25 17:02:33 +0200

    events style

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,7 @@ class EventsController < ApplicationController
   def index
     @user=User.all
   end
+  def comments
+    @event=Event.where(:action_type=> "comments")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ UserActions::Application.routes.draw do
       resources :likes
     end
   end
-resources :users do
-  resources :events
-end
+#resources :users do
+ # resources :events
+#end
   get '/categories/:category_name/:id', to: 'pictures#show'
-
+  get 'events/comments', :controller => 'events', :action => 'comments'
 
   get "events/", to: 'events#index'
-  get "events/:user_id/comments", to: 'events#comments'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
